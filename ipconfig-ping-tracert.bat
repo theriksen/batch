a4d68de0efc2a80a10f563896c89af20df3a59f7
theriksen <thomaseriksenivat@gmail.com>
2021-02-27 21:55:38 +0100

    atom-commmit

--- ipconfig-ping-tracert.bat
+++ ipconfig-ping-tracert.bat
@@ -1,41 +1,32 @@
 @echo off
 ECHO.
 ECHO.
-ECHO  ----------------------------
-ECHO [ Initializing script part 1 ]
-ECHO  ----------------------------
+ECHO [ Initializing script part 1 of 4 ]
 ECHO [ ipconfig /all ]
-ECHO  ---------------
 ECHO.
 ECHO.
 ipconfig /all
 ECHO.
 ECHO.
-ECHO  ----------------------------
-ECHO [ Initializing script part 2 ]
-ECHO  ----------------------------
+ECHO [ Initializing script part 2 of 4 ]
 ECHO [ ping google.com ]
-ECHO  -----------------
 ECHO.
 ECHO.
 ping google.com
 ECHO.
 ECHO.
-ECHO  ----------------------------
-ECHO [ Initializing script part 3 ]
-ECHO  ----------------------------
+ECHO [ Initializing script part 3 of 4 ]
 ECHO [ tracert google.com ]
-ECHO  --------------------
 ECHO.
 ECHO.
 tracert google.com
 ECHO.
 ECHO.
-ECHO  ---------------------
-ECHO [ Script by theriksen ]
-ECHO  ---------------------
+ECHO [ Initializing script part 4 of 4 ]
+ECHO [ Dumping outcome to desktop/ip-ping-trace.txt]
+ECHO.
 ECHO [ Completed ]
-ECHO  -----------
+ECHO [ Script by theriksen ]
 ECHO.
 ECHO.
 PAUSE
