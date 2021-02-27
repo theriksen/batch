1ab8d34d1fd5c5fe8bdff325ac8ae439b9f8f97c
theriksen <thomaseriksenivat@gmail.com>
2021-02-27 23:17:04 +0100

    atom-commit

--- ipconfig-ping-tracert.bat
+++ ipconfig-ping-tracert.bat
@@ -7,36 +7,31 @@
 ::|   - ipconfig /all
 ::|   - ping google.com
 ::|   - tracert google.com (max 30 hops)
-::|   - dump outcome of all sessions to a .txt file on user desktop
 ::|___________________________________________________________________________
 
 if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
 cls
 ECHO.
 ECHO.
-ECHO [ Initializing script part 1 of 4 ]
+ECHO [ Initializing script part 1 of 3 ]
 ECHO [ ipconfig /all ]
 ECHO.
 ECHO.
 ipconfig /all
 ECHO.
 ECHO.
-ECHO [ Initializing script part 2 of 4 ]
+ECHO [ Initializing script part 2 of 3 ]
 ECHO [ ping google.com ]
 ECHO.
 ECHO.
 ping google.com
 ECHO.
 ECHO.
-ECHO [ Initializing script part 3 of 4 ]
+ECHO [ Initializing script part 3 of 3 ]
 ECHO [ tracert google.com ]
 ECHO.
 ECHO.
 tracert google.com
-ECHO.
-ECHO.
-ECHO [ Initializing script part 4 of 4 ]
-ECHO [ Dumping outcome to desktop/ip-ping-trace.txt]
 ECHO.
 ECHO.
 ECHO [ Script completed ]
